b0bf9d6ad5f36cea90e16fc9cebe29ed1893df80
Noizzee <37045144+Noizzee@users.noreply.github.com>
2019-01-07 12:43:18 +0100

    added alert box

--- app.js
+++ app.js
@@ -14,8 +14,6 @@ let bookMarks = localStorage.getItem('bookmark') ? JSON.parse(localStorage.getIt
 localStorage.setItem('bookmark', JSON.stringify(bookMarks));
 const localBook = JSON.parse(localStorage.getItem('bookmark'));
 
-let howMany = 0; //Keep track how many bookmarks are in the list
-
 //Checking of something is entered in the inputfields
 const check = () => {
   if (userInputW.value.length > Number(0) && userInputU.value.length > Number(0)) {
@@ -26,9 +24,9 @@ const check = () => {
 };
 
 //Check if user entered a url
-const validURL = (string) => {
+const validURL = (URL) => {
   regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
-  if (regexp.test(string)) {
+  if (regexp.test(URL)) {
     return isValidURL = true;
   } else {
     return isValidURL = false;
@@ -51,19 +49,30 @@ const createBookmark = (input, input2) => { //accepts 2 parameters
   tdElement.parentElement.appendChild(tdElement2);
   tdElement.parentElement.appendChild(tdElement3);
   tableBody.appendChild(trElement);
-  howMany++;
 };
 
-//Function to delete a bookmark
+//deleteBookmark function
 const deleteBookmark = (event) => {
   let deleteButtons = document.querySelectorAll('.btn-danger');
-  deleteButtons.forEach((e, index) => {
+  deleteButtons.forEach((e, index) => { //Index value of elements to be used to splice the array
     if (event.target === e) { //e = the clicked button element
       tableBody.removeChild(e.parentElement.parentElement); //is the tr element, after the button you have td first and then tr
       bookMarks.splice(index, 1);
       localStorage.setItem('bookmark', JSON.stringify(bookMarks));
     }
   });
+};
+
+//Alertmessage function
+const showAlert = (message) => {
+  const alertDiv = document.createElement('div');
+  alertDiv.className = 'alert alert-warning';
+  alertDiv.appendChild(document.createTextNode(message));
+  const mForm = document.querySelector('#book-form-url');
+  mForm.insertBefore(alertDiv, submitBtn);
+  setTimeout(() => {
+    document.querySelector('.alert').remove()
+  }, 2000); //Removes the alertDiv after 2s
 };
 
 // Submit button to add the bookmark
@@ -84,6 +93,8 @@ submitBtn.addEventListener('click', (event) => {
     deleteBtn.forEach(special => {
       special.addEventListener('click', deleteBookmark);
     });
+  } else {
+    showAlert("Please enter a valid URL and websitename!")
   }
 });
 
@@ -91,5 +102,9 @@ submitBtn.addEventListener('click', (event) => {
 window.addEventListener('load', (event) => {
   localBook.forEach(item => {
     createBookmark(item.Website, item.URL);
+  });
+  let deleteBtn = document.querySelectorAll('.btn-danger'); //Adding eventlistener to all deleteBtn on load
+  deleteBtn.forEach(special => {
+    special.addEventListener('click', deleteBookmark);
   });
 });
